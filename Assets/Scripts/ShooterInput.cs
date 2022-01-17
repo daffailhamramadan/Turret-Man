9f4dc6e611c008270109a725addc96bb2a971391
Daffa Ilham Ramadan <daffailhamramadan127@gmail.com>
2022-01-17 09:35:11 +0700

    Add new Input A  or RightArrow and D or LeftArrow

--- Assets/Scripts/ShooterInput.cs
+++ Assets/Scripts/ShooterInput.cs
@@ -4,5 +4,14 @@ using UnityEngine;
 
 public class ShooterInput : MonoBehaviour
 {
-   
+    public bool RotationRight()
+    {
+        return Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow);
+    }
+
+    public bool RotationLeft()
+    {
+        return Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow);
+    }
+
 }
