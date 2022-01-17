766b1d0bc71fe88b676acff980596aa9d1125379
Daffa Ilham Ramadan <daffailhamramadan127@gmail.com>
2022-01-17 09:36:50 +0700

    RotateRight and RotateLeft z angle every 90f

--- Assets/Scripts/ShooterRotation.cs
+++ Assets/Scripts/ShooterRotation.cs
@@ -4,5 +4,23 @@ using UnityEngine;
 
 public class ShooterRotation : MonoBehaviour
 {
-    
+    ShooterInput shooterInput;
+
+    private void Awake()
+    {
+        shooterInput = GetComponent<ShooterInput>();
+    }
+
+    private void Update()
+    {
+        if (shooterInput.RotationRight())
+        {
+            transform.Rotate(0f, 0f, -90f);
+        }
+
+        if (shooterInput.RotationLeft())
+        {
+            transform.Rotate(0f, 0f, 90f);
+        }
+    }
 }
