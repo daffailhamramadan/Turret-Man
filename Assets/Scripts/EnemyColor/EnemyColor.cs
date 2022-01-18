edd57ddf0d078cacb014f9605794512c61d19a25
Daffa Ilham Ramadan <daffailhamramadan127@gmail.com>
2022-01-18 09:40:49 +0700

    Matching color

---
+++ Assets/Scripts/EnemyColor/EnemyColor.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyColor : MonoBehaviour
+{
+    [SerializeField] private Transform[] colorEnemyPosition;
+
+    [SerializeField] private GameObject[] colorEnemyPrefabs;
+
+    private List<Vector2> enemyDirection = new List<Vector2>();
+
+    [SerializeField] private float speed;
+
+    private GameObject colorEnemyPrefab;
+
+    private void Start()
+    {
+        InvokeRepeating("EnemyColorAppear", 1f, 1f);
+
+        InvokeRepeating("RandomRotate", 1f, 1f);
+    }
+
+    private void RandomRotate()
+    {
+        transform.Rotate(0f, 0f, -90f);
+    }
+
+    private void EnemyColorAppear()
+    {
+        enemyDirection.Insert(0, transform.up);
+
+        enemyDirection.Insert(1, -transform.up);
+
+        enemyDirection.Insert(2, transform.right);
+
+        enemyDirection.Insert(3, -transform.right);
+
+        for (int i = 0; i < colorEnemyPosition.Length; i++)
+        {
+            colorEnemyPrefab = Instantiate(colorEnemyPrefabs[i], colorEnemyPosition[i].position, Quaternion.identity);
+
+            Rigidbody2D rigidbody = colorEnemyPrefab.GetComponent<Rigidbody2D>();
+
+            rigidbody.velocity = enemyDirection[i] * speed;
+        }
+        
+    }
+}
---
+++ Assets/Scripts/EnemyColor/EnemyColorPrefab/EnemyColorCollide.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyColorCollide : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+
+    private Color32 color;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Shooter"))
+        {
+            Destroy(this.gameObject);
+        }
+
+        if (collision.CompareTag("Bullet") && GameObject.FindGameObjectWithTag("Bullet").GetComponent<SpriteRenderer>().color == spriteRenderer.color)
+        {
+            Destroy(this.gameObject);
+        }
+
+        
+    }
+}
